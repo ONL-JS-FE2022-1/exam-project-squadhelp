d0a080df88b5347ae89273216366b11cb5e71c53
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-04-04 21:20:43 +0300

    Fix image uploading (need review)

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,22 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 require('./dbMongo/mongoose');
 const router = require('./router');
 const controller = require('./socketInit');
 const handlerError = require('./handlerError/handler');
+const multerErrorHandler = require('./handlerError/multerHandler');
+const {FILES_PATH} = require('./constants');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/public', express.static('public'));
+app.use('/public', express.static(path.join(FILES_PATH)));
 app.use(router);
+app.use(multerErrorHandler);
 app.use(handlerError);
 
 const server = http.createServer(app);
--- server/src/utils/fileUpload.js
+++ server/src/utils/fileUpload.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const ServerError = require('../errors/ServerError');
 const { FILES_PATH } = require('../constants');
 const env = process.env.NODE_ENV || 'development';
-const devFilePath = path.resolve(__dirname, '..', '..', '..', 'public/images');
+const devFilePath = path.resolve(FILES_PATH, 'images');
 
 const filePath = env === 'production'
   ? '/var/www/html/images/'
@@ -25,55 +25,10 @@ const storageContestFiles = multer.diskStorage({
   },
 });
 
-const uploadAvatars = multer({ storage: storageContestFiles }).single('file');
-const uploadContestFiles = multer({ storage: storageContestFiles }).array(
-  'files', 3);
-const updateContestFile = multer({ storage: storageContestFiles }).single(
-  'file');
-const uploadLogoFiles = multer({ storage: storageContestFiles }).single(
-  'offerData');
+const multerInstance = multer({storage: storageContestFiles})
 
-module.exports.uploadAvatar = (req, res, next) => {
-  uploadAvatars(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      next(new ServerError());
-    } else if (err) {
-      next(new ServerError());
-    }
-    return next();
-  });
-};
-
-module.exports.uploadContestFiles = (req, res, next) => {
-  uploadContestFiles(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      next(new ServerError());
-    } else if (err) {
-      next(new ServerError());
-    }
-    return next();
-  });
-};
-
-module.exports.updateContestFile = (req, res, next) => {
-  updateContestFile(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      next(new ServerError());
-    } else if (err) {
-      next(new ServerError());
-    }
-    return next();
-  });
-};
-
-module.exports.uploadLogoFiles = (req, res, next) => {
-  uploadLogoFiles(req, res, (err) => {
-    if (err instanceof multer.MulterError) {
-      next(new ServerError());
-    } else if (err) {
-      next(new ServerError());
-    }
-    return next();
-  });
-};
+module.exports.uploadAvatar = multerInstance.single('file');
+module.exports.uploadContestFiles = multerInstance.array('files', 3);
+module.exports.updateContestFile = multerInstance.single('file');
+module.exports.uploadLogoFiles = multerInstance.single('offerData');
 
